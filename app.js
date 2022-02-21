41581cefece2bed8a0c7f0c4753f3d79ce5e7e8d
David Astvatsatryan <davidas@synopsys.com>
2022-02-21 22:23:29 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const errorController = require('./controllers/error.js');
+const mongoConnect = require('./helper/database').mongoConnect;
+
+const root = require('./helper/path.js');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(root, 'public')));
+
+app.use((req, res, next) => {
+  // User.findByPk(1)
+  // .then(user => {
+  //   req.user = user;
+  //   next();
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  // });
+  next();
+})
+
+app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(3000);
+})
+
