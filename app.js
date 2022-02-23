3b6a1e01d6cbf77a5da6a23ee6c2c8613250967a
David Astvatsatryan <davidastyan@gmail.com>
2022-02-23 22:33:46 +0400

    Added functionality with User and Cart

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const errorController = require('./controllers/error.js');
-const mongoConnect = require('./helper/database').mongoConnect;
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const errorController = require("./controllers/error.js");
+const mongoConnect = require("./helper/database").mongoConnect;
+const User = require("./models/user");
 
-const root = require('./helper/path.js');
+const root = require("./helper/path.js");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin.js');
-const shopRoutes = require('./routes/shop.js');
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(root, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(root, "public")));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  // .then(user => {
-  //   req.user = user;
-  //   next();
-  // })
-  // .catch(err => {
-  //   console.log(err);
-  // });
-  next();
-})
+  User.findById("62165874e5608d8b390d53e1")
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use(shopRoutes);
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 
 app.use(errorController.get404);
 
 mongoConnect(() => {
   app.listen(3000);
-})
-
+});
