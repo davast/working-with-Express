562a5690e8eca8b00c420908e51e14879d2b7d83
David Astvatsatryan <davidastyan@gmail.com>
2022-03-10 21:46:20 +0400

    \sessions and cookies finaized

--- app.js
+++ app.js
@@ -38,7 +38,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  User.findById("62249ffd84336f61ea0ad1a3")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
